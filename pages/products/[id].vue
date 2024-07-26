6e42785ee5aeeaa71374a2403ec6f723ff96d73b
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-26 14:33:30 -0700

    Create a page that uses routing params

---
+++ pages/products/[id].vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+const { id } = useRoute().params
+</script>
+<template>
+  <div>
+    Product details for: {{ id }}
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus atque dicta aut voluptates distinctio corporis quos, omnis, ipsa ratione tempora voluptate autem ipsum dolore deleniti facilis nobis aspernatur alias magni?</p>
+  </div>
+</template>
+
+<style scoped></style>
