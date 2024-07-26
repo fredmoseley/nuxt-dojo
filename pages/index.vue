ea2f156bd8858bb787fc456474c2013e73585656
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-26 14:32:59 -0700

    Create pages for our site

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+<template>
+  <div>
+    <h2>Home</h2>
+    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem nesciunt facilis incidunt necessitatibus voluptatum quae vitae earum fugit, nam molestias maiores cupiditate rerum, labore iure sed eaque quidem at! Rem! </p>
+    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem nesciunt facilis incidunt necessitatibus voluptatum quae vitae earum fugit, nam molestias maiores cupiditate rerum, labore iure sed eaque quidem at! Rem! </p>
+  </div>
+</template>
+
+<style  scoped >
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
