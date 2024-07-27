2adf3cf84c8fd14b2a0b1c266bb727a62cc90e4f
fmoseley <fredmoseley@stanfordalumni.org>
2024-07-27 13:13:39 -0700

    Add product details card

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -5,15 +5,11 @@ const { id } = useRoute().params;
 definePageMeta({
     layout: 'products'
 });
-
 const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`);
 </script>
 <template>
     <div>
-        Product details for: {{ id }}
-        <p>{{ product.title }}</p>
-        <p>{{ product.price }}</p>
-        <p>{{ product.id }}</p>
+        <ProductDetails :product="product" />
     </div>
 </template>
 
